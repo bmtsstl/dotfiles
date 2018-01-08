68438df88fb8f25aa21843b0f8dec255de69e751
ryouser <35141868+ryouser@users.noreply.github.com>
2018-01-08 20:29:25 +0900

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -eu
+
+DOTFILES="${HOME}/.dotfiles"
+GIT_REPOSITORY='https://github.com/ryouser/dotfiles.git'
+ZIP_URL='https://github.com/ryouser/dotfiles/archive/master.zip'
+
+if ! which git; then
+	echo 'git is not installed or not in the $PATH' >&2
+	exit 1
+fi
+
+git clone --recursive "${GIT_REPOSITORY}" "${DOTFILES}"
+cd "${DOTFILES}"
+git config user.name 'ryouser'
+git config user.email '35141868+ryouser@users.noreply.github.com'
+
+"${DOTFILES}/deploy"

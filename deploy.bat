65755a431d046edd403491f5e3723c184edfc5b5
ryouser <35141868+ryouser@users.noreply.github.com>
2018-11-30 10:30:36 +0900

    use copy when mklink fails on Windows

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,10 @@
 @echo off
 mkdir "%USERPROFILE%\vimfiles"
+
+del "%USERPROFILE%\vimfiles\vimrc"
 mklink "%USERPROFILE%\vimfiles\vimrc" "%~dp0dotfiles\vimrc"
+if %ERRORLEVEL% NEQ 0 copy "%~dp0dotfiles\vimrc" "%USERPROFILE%\vimfiles\vimrc"
+
+del "%USERPROFILE%\vimfiles\gvimrc"
 mklink "%USERPROFILE%\vimfiles\gvimrc" "%~dp0dotfiles\gvimrc"
+if %ERRORLEVEL% NEQ 0 copy "%~dp0dotfiles\gvimrc" "%USERPROFILE%\vimfiles\gvimrc"

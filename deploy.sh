28fe25f605319f37fb4b4607d305094c0d31c6b6
bmtsstl <35141868+bmtsstl@users.noreply.github.com>
2019-02-16 19:43:02 +0900

    fix handling filename with spaces

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 DOTFILES="$(cd $(dirname $0); pwd)/dotfiles"
 
 mkdir -pm 700 ~/.vim
-ln --backup --symbolic ${DOTFILES}/vimrc ~/.vim/vimrc
-ln --backup --symbolic ${DOTFILES}/gvimrc ~/.vim/gvimrc
+ln --backup --symbolic "${DOTFILES}/vimrc" ~/.vim/vimrc
+ln --backup --symbolic "${DOTFILES}/gvimrc" ~/.vim/gvimrc

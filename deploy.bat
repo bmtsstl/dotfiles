7258b32c850a57d8289a8c35b96af1903ea4ccac
bmtsstl <35141868+bmtsstl@users.noreply.github.com>
2019-02-16 19:25:48 +0900

    force overwrite on windows

--- deploy.bat
+++ deploy.bat
@@ -1,10 +1,10 @@
 @echo off
 mkdir "%HOMEDRIVE%%HOMEPATH%\vimfiles"
 
-move "%HOMEDRIVE%%HOMEPATH%\vimfiles\vimrc" "%HOMEDRIVE%%HOMEPATH%\vimfiles\vimrc~"
+move /Y "%HOMEDRIVE%%HOMEPATH%\vimfiles\vimrc" "%HOMEDRIVE%%HOMEPATH%\vimfiles\vimrc~"
 mklink "%HOMEDRIVE%%HOMEPATH%\vimfiles\vimrc" "%~dp0dotfiles\vimrc"
-if %ERRORLEVEL% NEQ 0 copy "%~dp0dotfiles\vimrc" "%HOMEDRIVE%%HOMEPATH%\vimfiles\vimrc"
+if %ERRORLEVEL% NEQ 0 copy /Y "%~dp0dotfiles\vimrc" "%HOMEDRIVE%%HOMEPATH%\vimfiles\vimrc"
 
-move "%HOMEDRIVE%%HOMEPATH%\vimfiles\gvimrc" "%HOMEDRIVE%%HOMEPATH%\vimfiles\gvimrc~"
+move /Y "%HOMEDRIVE%%HOMEPATH%\vimfiles\gvimrc" "%HOMEDRIVE%%HOMEPATH%\vimfiles\gvimrc~"
 mklink "%HOMEDRIVE%%HOMEPATH%\vimfiles\gvimrc" "%~dp0dotfiles\gvimrc"
-if %ERRORLEVEL% NEQ 0 copy "%~dp0dotfiles\gvimrc" "%HOMEDRIVE%%HOMEPATH%\vimfiles\gvimrc"
+if %ERRORLEVEL% NEQ 0 copy /Y "%~dp0dotfiles\gvimrc" "%HOMEDRIVE%%HOMEPATH%\vimfiles\gvimrc"

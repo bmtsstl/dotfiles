f287e10b661038cdbf8f4a2e87980856d2e72e68
bmtsstl <35141868+bmtsstl@users.noreply.github.com>
2019-02-16 19:21:07 +0900

    make a backup of existing file on windows

--- deploy.bat
+++ deploy.bat
@@ -1,10 +1,10 @@
 @echo off
 mkdir "%HOMEDRIVE%%HOMEPATH%\vimfiles"
 
-del "%HOMEDRIVE%%HOMEPATH%\vimfiles\vimrc"
+move "%HOMEDRIVE%%HOMEPATH%\vimfiles\vimrc" "%HOMEDRIVE%%HOMEPATH%\vimfiles\vimrc~"
 mklink "%HOMEDRIVE%%HOMEPATH%\vimfiles\vimrc" "%~dp0dotfiles\vimrc"
 if %ERRORLEVEL% NEQ 0 copy "%~dp0dotfiles\vimrc" "%HOMEDRIVE%%HOMEPATH%\vimfiles\vimrc"
 
-del "%HOMEDRIVE%%HOMEPATH%\vimfiles\gvimrc"
+move "%HOMEDRIVE%%HOMEPATH%\vimfiles\gvimrc" "%HOMEDRIVE%%HOMEPATH%\vimfiles\gvimrc~"
 mklink "%HOMEDRIVE%%HOMEPATH%\vimfiles\gvimrc" "%~dp0dotfiles\gvimrc"
 if %ERRORLEVEL% NEQ 0 copy "%~dp0dotfiles\gvimrc" "%HOMEDRIVE%%HOMEPATH%\vimfiles\gvimrc"

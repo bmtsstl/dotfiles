1a14ca44374fd3ecc3b15626becbe4f9c0102d78
bmtsstl <35141868+bmtsstl@users.noreply.github.com>
2019-02-17 10:31:21 +0900

    add bashrc

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 DOTFILES="$(cd $(dirname $0); pwd)/dotfiles"
 
 ln --backup --symbolic "${DOTFILES}/bash_profile" ~/.bash_profile
+ln --backup --symbolic "${DOTFILES}/bashrc" ~/.bashrc
 
 mkdir -pm 700 ~/.vim
 ln --backup --symbolic "${DOTFILES}/vimrc" ~/.vim/vimrc

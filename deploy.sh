928236cefbc7cc6cbac1dccce3eb002c554a7d75
bmtsstl <35141868+bmtsstl@users.noreply.github.com>
2019-02-20 10:01:04 +0900

    add tmux.conf

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ DOTFILES="$(cd $(dirname $0); pwd)/dotfiles"
 
 ln --backup --symbolic "${DOTFILES}/bash_profile" ~/.bash_profile
 ln --backup --symbolic "${DOTFILES}/bashrc" ~/.bashrc
+ln --backup --symbolic "${DOTFILES}/tmux.conf" ~/.tmux.conf
 
 mkdir -pm 700 ~/.vim
 ln --backup --symbolic "${DOTFILES}/vimrc" ~/.vim/vimrc

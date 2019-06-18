e6625e9f2686429dbb5da22a0905a2d8152ea555
bmtsstl <35141868+bmtsstl@users.noreply.github.com>
2019-06-18 11:07:48 +0900

    use copy instead of move for backup on windows

--- deploy.bat
+++ deploy.bat
@@ -1,12 +1,14 @@
 @echo off
 mkdir "%HOMEDRIVE%%HOMEPATH%\vimfiles"
 
-move /Y "%HOMEDRIVE%%HOMEPATH%\vimfiles\vimrc" "%HOMEDRIVE%%HOMEPATH%\vimfiles\vimrc~"
+copy /V /Y "%HOMEDRIVE%%HOMEPATH%\vimfiles\vimrc" "%HOMEDRIVE%%HOMEPATH%\vimfiles\vimrc~"
+del "%HOMEDRIVE%%HOMEPATH%\vimfiles\vimrc"
 mklink "%HOMEDRIVE%%HOMEPATH%\vimfiles\vimrc" "%~dp0dot\vimrc"
 if %ERRORLEVEL% NEQ 0 mklink /H "%HOMEDRIVE%%HOMEPATH%\vimfiles\vimrc" "%~dp0dot\vimrc"
 if %ERRORLEVEL% NEQ 0 copy /Y "%~dp0dot\vimrc" "%HOMEDRIVE%%HOMEPATH%\vimfiles\vimrc"
 
-move /Y "%HOMEDRIVE%%HOMEPATH%\vimfiles\gvimrc" "%HOMEDRIVE%%HOMEPATH%\vimfiles\gvimrc~"
+copy /V /Y "%HOMEDRIVE%%HOMEPATH%\vimfiles\gvimrc" "%HOMEDRIVE%%HOMEPATH%\vimfiles\gvimrc~"
+del "%HOMEDRIVE%%HOMEPATH%\vimfiles\gvimrc"
 mklink "%HOMEDRIVE%%HOMEPATH%\vimfiles\gvimrc" "%~dp0dot\gvimrc"
 if %ERRORLEVEL% NEQ 0 mklink /H "%HOMEDRIVE%%HOMEPATH%\vimfiles\gvimrc" "%~dp0dot\gvimrc"
 if %ERRORLEVEL% NEQ 0 copy /Y "%~dp0dot\gvimrc" "%HOMEDRIVE%%HOMEPATH%\vimfiles\gvimrc"

0994e76e4f1fcfd408ab0c4a5fbb9d2940aa2396
bmtsstl <35141868+bmtsstl@users.noreply.github.com>
2019-06-18 10:51:04 +0900

    use hardlink if available on windows

--- deploy.bat
+++ deploy.bat
@@ -3,8 +3,10 @@ mkdir "%HOMEDRIVE%%HOMEPATH%\vimfiles"
 
 move /Y "%HOMEDRIVE%%HOMEPATH%\vimfiles\vimrc" "%HOMEDRIVE%%HOMEPATH%\vimfiles\vimrc~"
 mklink "%HOMEDRIVE%%HOMEPATH%\vimfiles\vimrc" "%~dp0dot\vimrc"
+if %ERRORLEVEL% NEQ 0 mklink /H "%HOMEDRIVE%%HOMEPATH%\vimfiles\vimrc" "%~dp0dot\vimrc"
 if %ERRORLEVEL% NEQ 0 copy /Y "%~dp0dot\vimrc" "%HOMEDRIVE%%HOMEPATH%\vimfiles\vimrc"
 
 move /Y "%HOMEDRIVE%%HOMEPATH%\vimfiles\gvimrc" "%HOMEDRIVE%%HOMEPATH%\vimfiles\gvimrc~"
 mklink "%HOMEDRIVE%%HOMEPATH%\vimfiles\gvimrc" "%~dp0dot\gvimrc"
+if %ERRORLEVEL% NEQ 0 mklink /H "%HOMEDRIVE%%HOMEPATH%\vimfiles\gvimrc" "%~dp0dot\gvimrc"
 if %ERRORLEVEL% NEQ 0 copy /Y "%~dp0dot\gvimrc" "%HOMEDRIVE%%HOMEPATH%\vimfiles\gvimrc"

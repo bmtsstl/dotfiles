98d4efbbd8626421f5a3a14f860b73990d646033
bmtsstl <35141868+bmtsstl@users.noreply.github.com>
2021-07-22 23:08:40 +0900

    don't overwrite .bashrc and .bash_profile

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 DOT="$(cd $(dirname $0); pwd)/dot"
 
-ln --backup --symbolic "${DOT}/bash_profile" ~/.bash_profile
-ln --backup --symbolic "${DOT}/bashrc" ~/.bashrc
+ln --backup --symbolic "${DOT}/profile_remote" ~/.profile_remote
+ln --backup --symbolic "${DOT}/bashrc_remote" ~/.bashrc_remote
 ln --backup --symbolic "${DOT}/tmux.conf" ~/.tmux.conf
 
 mkdir -pm 700 ~/.vim

2a73539520e4d89586400eac3aa5b75d0c47ba6f
bmtsstl <35141868+bmtsstl@users.noreply.github.com>
2025-10-21 22:52:22 +0900

    sh: rename bashrc_remote to shellrc

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 DOT="$(cd $(dirname $0); pwd)/dot"
 
-ln --backup --symbolic "${DOT}/bashrc_remote" ~/.bashrc_remote
+ln --backup --symbolic "${DOT}/shellrc" ~/.shellrc
 ln --backup --symbolic "${DOT}/tmux.conf" ~/.tmux.conf
 
 mkdir -pm 700 ~/.vim

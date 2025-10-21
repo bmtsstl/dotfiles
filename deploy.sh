52f20d2ec778672bbaea0574162fedbd004b00a4
bmtsstl <35141868+bmtsstl@users.noreply.github.com>
2025-10-21 22:26:55 +0900

    bash: remove .profile_remote

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #/bin/bash
 DOT="$(cd $(dirname $0); pwd)/dot"
 
-ln --backup --symbolic "${DOT}/profile_remote" ~/.profile_remote
 ln --backup --symbolic "${DOT}/bashrc_remote" ~/.bashrc_remote
 ln --backup --symbolic "${DOT}/tmux.conf" ~/.tmux.conf
 
